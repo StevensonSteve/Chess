70935fcd174892789840be90bcfc8c490838be02
Stevenson <stevensonsteve393@gmail.com>
2026-02-11 18:24:35 +0300

    Сделал читабельным main()

--- main.cpp
+++ main.cpp
@@ -8,89 +8,74 @@ const int BOARD_SIZE = 8;
 const float SQUARE_SIZE = 100.f;
 using ChessBoard = std::array<std::array<char, BOARD_SIZE>, BOARD_SIZE>;
 
-char getSquareColor(int x, int y) {
-    if ((x + y) % 2 == 0) {
-        return 'W';
-    } else {
-        return 'B';
-    }    
-}
-
-ChessBoard initChessboard() {
-    ChessBoard chessboard;
-
+void drawChessboard(sf::RenderWindow& window) {
     for (int i = 0; i < BOARD_SIZE; i++) {
         for (int j = 0; j < BOARD_SIZE; j++) {
-            chessboard[i][j] = getSquareColor(i, j);
+            sf::RectangleShape rect(sf::Vector2f(SQUARE_SIZE, SQUARE_SIZE));
+            rect.setPosition(i * SQUARE_SIZE, j * SQUARE_SIZE);
+
+            if ((i + j) % 2 == 0) {
+                rect.setFillColor(sf::Color::White);
+            } else {
+                rect.setFillColor(sf::Color(100, 100, 100));
+            }  
+
+            window.draw(rect);
         }
     }
-
-    return chessboard;
 }
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 800), "Chess Board");
-    float size = SQUARE_SIZE;
-    ChessBoard chessboard;
 
-    chessboard = initChessboard();
+void eventProcess(sf::RenderWindow& window, ChessBoard &chessboard) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+        if (event.type == sf::Event::Closed) 
+            window.close();
+    }
 
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) 
-                window.close();
+
+    if (event.type == sf::Event::MouseButtonPressed) {
+        int x = event.mouseButton.x / (int)SQUARE_SIZE;
+        int y = event.mouseButton.y / (int)SQUARE_SIZE;
+
+        if (event.mouseButton.button == sf::Mouse::Left) {
+            chessboard[x][y] = 'R';
+        } else if (event.mouseButton.button == sf::Mouse::Right) {
+            chessboard[x][y] = (x + y) % 2 == 0 ? 'W' : 'B';
         }
+    }     
+}
 
 
-        if (event.type == sf::Event::MouseButtonPressed) {
-            if (event.mouseButton.button == sf::Mouse::Left) {
-                int x = event.mouseButton.x / (int)size;
-                int y = event.mouseButton.y / (int)size;
-                // cout << "x: " << event.mouseButton.x << " y: " << event.mouseButton.y << "\n";
-                chessboard[x][y] = 'R';
-            }
-        }
-
-        if (event.type == sf::Event::MouseButtonPressed) {
-            if (event.mouseButton.button == sf::Mouse::Right) {
-                int x = event.mouseButton.x / (int)size;
-                int y = event.mouseButton.y / (int)size;
-                // cout << "x: " << event.mouseButton.x << " y: " << event.mouseButton.y << "\n";
-                chessboard[x][y] = getSquareColor(x, y);
-            }
-        }        
-
+void setFigures(sf::RenderWindow& window, ChessBoard &chessboard) {
         for (int i = 0; i < BOARD_SIZE; i++) {
             for (int j = 0; j < BOARD_SIZE; j++) {
-
-                if (chessboard[i][j] == 'W' || chessboard[i][j] == 'B') {
-                    sf::RectangleShape rect(sf::Vector2f(size, size));
-                    rect.setPosition(i * size, j * size);
-                    switch (chessboard[i][j]) {
-                        case 'W':
-                            rect.setFillColor(sf::Color::White);
-                            break;
-                        case 'B':
-                        rect.setFillColor(sf::Color(100, 100, 100));
-                            break;
-                    // default:
-                    //     std::cout << "This is a middle row (2-5)." << std::endl;
-                    //     break;
-                    }
-                    window.draw(rect);
-                } else {
+                if (chessboard[i][j] == 'R') {
                     sf::CircleShape circle(SQUARE_SIZE / 4); 
-                    circle.setPosition(i * size + SQUARE_SIZE / 4, j * size + SQUARE_SIZE / 4);
+                    circle.setPosition(i * SQUARE_SIZE + SQUARE_SIZE / 4, j * SQUARE_SIZE + SQUARE_SIZE / 4);
                     circle.setFillColor(sf::Color::Red);
                     window.draw(circle);
                 }
             }
 
         }
+}
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 800), "Chess Board");
+    ChessBoard chessboard;
+
+    while (window.isOpen()) {
+
+        eventProcess(window, chessboard);
+    
+        window.clear();
+
+        drawChessboard(window);
+        setFigures(window, chessboard);
 
         window.display();
-        window.clear();
+
     }
 
     return 0;
